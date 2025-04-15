d138bdf83570e94095fcad2e7bcd53903e5e4a01
Alejandra Gutierrez <alexmarket43@gmail.com>
2025-04-14 20:29:12 -0700

    Implement logic for exercise #5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,7 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+puts "Stores total revenue: #{Store.sum(:annual_revenue)}"
+puts "Stores average revenue: #{Store.average(:annual_revenue)}"
+puts "Stores with over $1M of revenue: #{Store.where('annual_revenue >= ?', 1000000).count}"
