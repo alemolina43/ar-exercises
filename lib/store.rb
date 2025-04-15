712b7703631c25ff7de6d4daf182af6b6b5909a5
Alejandra Gutierrez <alexmarket43@gmail.com>
2025-04-14 20:32:25 -0700

    implement logic for exercise #7

--- lib/store.rb
+++ lib/store.rb
@@ -1,5 +1,14 @@
 class Store < ActiveRecord::Base
   has_many :employees
 
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { greater_than: 0 }
+  validate :must_carry_apparel
+
+  def must_carry_apparel
+    unless mens_apparel || womens_apparel
+      errors.add(:base, "Store must carry at least one of the men's or women's apparel.")
+    end
+  end
 
 end
